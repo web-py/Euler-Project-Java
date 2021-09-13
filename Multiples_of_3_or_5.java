590f17415ae2453ef75244975cf44057b76b3dbf
Mohammad Rostami <mr.rostami94@gmail.com>
2021-09-13 16:13:37 +0430

    Solve 01 Problem- Multiples of 3 or 5

---
+++ Multiples_of_3_or_5.java
@@ -0,0 +1,15 @@
+/**
+ * 01-Multiples_of_3_or_5
+ */
+public class Multiples_of_3_or_5 {
+    
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 0; i < 1000; i++) {
+            if (i%3 == 0 | i%5 == 0) {
+                sum += i;
+            }
+        }
+        System.out.println(sum);
+    }
+}
